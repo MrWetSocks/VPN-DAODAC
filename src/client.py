f545276779082080788ab84377eabae03f544c50
Oliver Akatugba <oliver.akatugba@gmail.com>
2023-02-07 15:04:10 +0100

    WOrked on stuff

--- src/client.py
+++ src/client.py
@@ -1,28 +1,40 @@
 import requests
 import socket
+import os
+import webbrowser
+import sys
 
-HOST = "127.0.0.1"
-PORT = 12346
-
+LOCAL_HOST = "127.0.0.1"
+LOCAL_PORT = 12345
+if len(sys.argv) == 2:
+    HOST, PORT = sys.argv[-1].split(':')
+    PORT = int(PORT)
+print(sys.argv)
 def receive_request():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
+        s.bind((LOCAL_HOST, LOCAL_PORT))
         s.listen()
         conn, addr = s.accept()
         with conn:
             print(f"Connected by {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+            with open('index.html', 'w') as f:
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    f.write(data)
 
-def send_request():
+                # conn.sendall(data)
+            webbrowser.open('file://' + os.getcwd() + '/index.html', new=2)
+
+def send_request(dat):
+    print('hej')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(b"Hello, world")
-        data = s.recv(1024)
+        print(f"Connected to {HOST}:{PORT}")
+        s.sendall(dat)
     
-        print(f"Received {data!r}")
+    return
 
+send_request(b"lkajslkjflkj")
 receive_request()
