ed023fa77c3144a0f98ed1c32cc255c4f1baf3d6
Hugo Lindström <huggepugge1@gmail.com>
2023-02-08 10:31:52 +0100

    removed client side server

--- src/client.py
+++ src/client.py
@@ -5,36 +5,27 @@ import webbrowser
 import sys
 
 LOCAL_HOST = "127.0.0.1"
-LOCAL_PORT = 12345
+LOCAL_PORT = 12346
 if len(sys.argv) == 2:
     HOST, PORT = sys.argv[-1].split(':')
     PORT = int(PORT)
 print(sys.argv)
-def receive_request():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((LOCAL_HOST, LOCAL_PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            with open('index.html', 'w') as f:
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    f.write(data)
 
-                # conn.sendall(data)
-            webbrowser.open('file://' + os.getcwd() + '/index.html', new=2)
 
-def send_request(dat):
-    print('hej')
+def request(data):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         print(f"Connected to {HOST}:{PORT}")
-        s.sendall(dat)
-    
-    return
+        s.sendall(data)
+        with open('index.html', 'w', encoding="ISO-8859-1") as f:
+            while True:
+                recv = s.recv(1024)
+                print(recv)
+                if not recv or recv == b"":
+                    break
+                f.write(recv.decode("ISO-8859-1"))
 
-send_request(b"lkajslkjflkj")
-receive_request()
+        webbrowser.open('file://' + os.getcwd() + '/index.html', new=2)
+
+
+request(b"Hello World!")
--- src/server.py
+++ src/server.py
@@ -13,14 +13,11 @@ async def loop():
             conn, addr = s.accept()
             with conn:
                 print(f"Connected to {addr}")
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    if data == b"exit\n":
-                        break
-                    conn.sendall(requests.get("https://google.com").content)
-                    conn.sendall(b"Message recieved\n")
-                    print(data.decode())
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(requests.get("https://google.com").content)
+                conn.close()
+                print(data.decode())
 
 asyncio.run(loop())
