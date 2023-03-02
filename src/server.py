25d9ae64cb796021c8bec86c221d755dc977b450
Hugo Lindström <huggepugge1@gmail.com>
2023-03-02 13:58:31 +0100

    final

--- src/server.py
+++ src/server.py
@@ -4,12 +4,12 @@ import requests
 import re
 import random
 
-HOST = "localhost"
+HOST = "192.168.201.162"
 PORT = random.randrange(20000, 30000)
 print(PORT)
 
 async def server():
-    with socket.socket() as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         while True:
